66377a3d9ffb520019f97bbdd5abd01f268935c5
leo978 <leoramirez978@gmail.com>
2018-10-04 23:52:43 -0500

    subi archivo

---
+++ app/Http/Controllers/AlumnoController.php
@@ -0,0 +1,23 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+class AlumnoController extends Controller
+{
+    public function index()
+    {
+    	return view('blanco');
+    }
+
+    public function create()
+    {
+    	return 'Ua';
+    }
+
+    public function store()
+    {
+    	return 'Uarios';
+    }
+}
